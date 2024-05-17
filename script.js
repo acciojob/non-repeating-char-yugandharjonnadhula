0c867936ed3b03056624940c4d8ee8d0d168403e
yugandharjonnadhula <163026370+yugandharjonnadhula@users.noreply.github.com>
2024-05-17 17:48:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
-}
+	const charcount={};
+	for(let char of str){
+		charcount[char]=(charcount[char] || 0)+1;
+	}
+	for(let char of str){
+		if(charcount[char]===1){
+			return char;
+		}
+	}
+	return null;
+	
+ }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
